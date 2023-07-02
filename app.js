07447ae527ae88840132c2c106e82833b6b39b4c
Shubham <105529358+Shubh99992@users.noreply.github.com>
2023-07-02 23:43:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Tutorial by http://youtube.com/CodeExplained
-// api key : 82005d27a116c2880c8f0fcb866998a0
+// api key : fd3f05e1c21bb97dbe010b2642c6b300
 
 // SELECT ELEMENTS
 const iconElement = document.querySelector(".weather-icon");
